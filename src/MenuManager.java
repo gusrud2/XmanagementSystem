da5aa2b9bd04eb154c4b9652cfb9b968c0e2d343
gusrud2 <gcyfj@naver.com>
2022-03-18 16:06:51 +0900

    add menu selection

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,12 +1,34 @@
+import java.util.Scanner;
 
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("1. Add Clothes");
-		System.out.println("2. Delete Clothes");
-		System.out.println("3. Edit Clothes");
-		System.out.println("4. View Clothes");
-		System.out.println("5. Exit");
+		int num = 0;
+		Scanner input = new Scanner(System.in);
+		
+		while(num != 6) {
+			System.out.println("*** Clothes Management System Menu ***");
+			System.out.println("1. Add Clothes");
+		    System.out.println("2. Delete Clothes");
+		    System.out.println("3. Edit Clothes");
+		    System.out.println("4. View Clothes");
+		    System.out.println("5. Show a Menu");
+		    System.out.println("6. Exit");
+		    System.out.print("Select one number between 1-6 : ");
+		    num = input.nextInt();
+		    
+		    if (num == 1) {
+		    	System.out.print("Clothes Category: ");
+		    	input.nextLine();
+		    	String category = input.nextLine();
+		    	System.out.print("Clothes Brand: ");
+		    	String brand = input.nextLine();
+		    	System.out.print("Clothes Color: ");
+		    	String color = input.nextLine();
+		    	System.out.print("Clothes Season: ");
+		    	String season = input.nextLine();
+		    }
+		}
 
 	}
 
