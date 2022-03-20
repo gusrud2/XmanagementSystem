c82690db472bd46ab50c1da5abcb097282fa5390
gusrud2 <gcyfj@naver.com>
2022-03-20 23:14:59 +0900

    add menu selection

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -17,17 +17,39 @@ public class MenuManager {
 		    System.out.print("Select one number between 1-6 : "); 
 		    num = input.nextInt();
 		    
-		    if (num == 1) {
-		    	System.out.print("Clothes Category: "); //옷 종류에 대해 입력받도록 출력
+		    switch (num) {
+		    case 1 :
+		    	System.out.print("Clothes Category: "); // 옷 종류에 대해 입력받도록 출력
 		    	input.nextLine();
-		    	String category = input.nextLine();
+		    	String category = input.nextLine(); 
 		    	System.out.print("Clothes Brand: "); //옷 브랜드에 대해 입력받도록 출력
 		    	String brand = input.nextLine();
 		    	System.out.print("Clothes Color: "); //옷 색깔에 대해 입력받도록 출력
 		    	String color = input.nextLine();
-		    	System.out.print("Clothes Season: "); // 봄, 여름, 가을, 겨울 등 입력한 옷을 입는 계쩔을 입력받도록 출력
+		    	System.out.print("Clothes Season: "); //봄, 여름, 가을, 겨울 등 입력한 옷에 맞는 계절을 입력하도록 출력
 		    	String season = input.nextLine();
+		    	break;
+		    	
+		    case 2 :
+		    	System.out.print("Clothes Category: "); 
+		    	input.nextLine();
+		    	String category2 = input.nextLine();
+		    	break;
+		    	
+		    case 3 :
+		    	System.out.print("Clothes Category: "); 
+		    	input.nextLine();
+		    	String category3 = input.nextLine();
+		    	break;
+		    	
+		    case 4 :
+		    	System.out.print("Clothes Category: "); 
+		    	input.nextLine();
+		    	String category4 = input.nextLine();
+		    	break;
+		    	
 		    }
+		    
 		    System.out.println();
 		}
 
