9920e30a99a315eaca96712b5cd5601ec2d99eff
gusrud2 <gcyfj@naver.com>
2022-04-02 23:13:10 +0900

    class

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class MenuManager {
 
 	public static void main(String[] args) {
-		int num = 0;
+
 		Scanner input = new Scanner(System.in);
+		ClothesManager ClothesManager = new ClothesManager(input);
+
+		int num = 0;
 		
 		while(num != 6) {
 			System.out.println("*** Clothes Management System Menu ***");
@@ -12,61 +15,27 @@ public class MenuManager {
 		    System.out.println("2. Delete Clothes");
 		    System.out.println("3. Edit Clothes");
 		    System.out.println("4. View Clothes");
-		    System.out.println("5. Show a Menu");
-		    System.out.println("6. Exit");
-		    System.out.print("Select one number between 1-6 : "); 
+		    System.out.println("5. Exit");
+		    System.out.print("Select one number between 1-5: "); 
 		    num = input.nextInt();
 		    
-		   if(num == 1) {
-			   AddClothes();
+		   if(num == 1) { //1일때
+			   ClothesManager.AddClothes(); //AddClothes 함수로 이동
 		    }
-		   else if (num == 2) {
-			   DeleteClothes();
+		   else if (num == 2) { //2일때
+			   ClothesManager.DeleteClothes(); //DeleteClothtes 함수로 이동
 			}
-		   else if (num == 3) {
-			   EditClothes();
+		   else if (num == 3) { //3일때
+			   ClothesManager.EditClothes(); //EditClothes 함수로 이동
 		   }
-		   else if (num == 4) {
-			   ViewClothes();
+		   else if (num == 4) { //4일때
+			   ClothesManager.ViewClothes(); //ViewClothes 함수로 이동
 		   }
 		   else {
-			   continue;
+			   continue; 
 		   }
 		}
 		
 	}
 	
-	public static void AddClothes() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Clothes Category: ");
-		String category = input.nextLine(); 
-		System.out.print("Clothes Brand: "); 
-		String brand = input.nextLine();
-		System.out.print("Clothes Color: "); 
-		String color = input.nextLine();
-		System.out.print("Clothes Season: "); 
-		String season = input.nextLine();
-		
-	}
-	
-	public static void DeleteClothes() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Clothes Category: ");
-		String category = input.nextLine();
-		
-	}
-	
-	public static void EditClothes() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Clothes Category: ");
-		String category = input.nextLine();
-		
-	}
-	
-	public static void ViewClothes() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Clothes Category: ");
-		String category = input.nextLine();
-		
-	}
 }
