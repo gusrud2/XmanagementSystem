8436daf98a3ff83b65e2781df799870a87c561b2
gusrud2 <101459095+gusrud2@users.noreply.github.com>
2023-02-01 11:09:27 +0900

    Update Bag.java

--- src/clothes/Bag.java
+++ src/clothes/Bag.java
@@ -2,6 +2,21 @@ package clothes;
 
 import java.util.Scanner;
 
+
+public class HBD{
+    public static void main(string[] args){
+        String name = "장배지우";
+        int birthday = 0122;
+
+        if (current_day == 0203){
+            System.out.println(name+"의 
+		     생일을 축하합니다!♥")
+        }
+    }
+}
+
+
+
 public class Bag extends Clothes {
 	
 	public Bag(ClothesKind kind) {
